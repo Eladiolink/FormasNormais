8b5bf5a7827e39f0c0f8e4a3ee62e367ec1c847a
Eládio Alves <eladiolink1@gmail.com>
2023-10-27 23:15:38 -0300

    Atualização pre chomsky

--- formasNormais/formaGreibach/formaGreibach.go
+++ formasNormais/formaGreibach/formaGreibach.go
@@ -1,1 +1,23 @@
 package formagreibach
+
+import (
+	"FormasNormais/helpers/gramatica"
+	"FormasNormais/helpers/simplificacao"
+	"fmt"
+)
+
+func Formaprechomsky(gramatica *gramatica.Gramatica){
+	simplificacao.Simplificacao(gramatica)
+
+	// RenomearVariaveis
+
+	fmt.Printf("GRAMÁTICA NA FORMA PRÉ-CHOMSKY!!! ʕ•́ᴥ•̀ʔっ \n")
+}
+
+func renomearVariaveis(gramatica *gramatica.Gramatica){
+	// renameMap := (make(map[string]string))
+
+	for chave,valor := range gramatica.V {
+		fmt.Println(chave,valor)
+	}
+}
